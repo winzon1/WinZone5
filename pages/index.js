a253a8eb4d729fd6980db7547901784f3c046115
winzon1 <nash48534@gmail.com>
2026-03-16 13:01:42 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <div style={{textAlign:"center",padding:"50px",fontFamily:"Arial"}}>
+      <h1>WinZone</h1>
+      <p>Your betting platform is starting...</p>
+
+      <button style={{
+        padding:"10px 20px",
+        background:"#16c784",
+        border:"none",
+        color:"white",
+        fontSize:"18px",
+        borderRadius:"5px"
+      }}>
+        View Matches
+      </button>
+
+    </div>
+  )
+                }
